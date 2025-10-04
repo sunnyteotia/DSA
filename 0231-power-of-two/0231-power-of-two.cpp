220a8cc7073a32e2b06393658f5e8a0c5109b092
Sunny Teotia <138746726+sunnyteotia@users.noreply.github.com>
2025-10-04 23:40:23 +0530

    Time: 0 ms (100%), Space: 7.9 MB (17.94%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-       while(n!=0){
-        int bit=n%2;
-        n/=2;
-        if(n==0 && bit==1)return true;
-        if(n!=0 && bit==1)return false;
-       }
-       return false;
+      return n > 0 && (n & (n - 1)) == 0;
     }
 };
